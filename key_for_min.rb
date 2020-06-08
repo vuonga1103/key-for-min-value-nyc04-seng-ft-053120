3fe0b18ced18ea49421b7d45bb56c0e8f482b685
Anh Vuong <vuonga1103@yahoo.com>
2020-06-08 20:01:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   smallest_value = nil
 
   name_hash.each do |key, value|
-    if lowest_value == nil || value < lowest_value
+    if smallest_value == nil || value < smallest_value
       smallest_key = key
       smallest_value = value
     end
