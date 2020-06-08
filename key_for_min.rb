e96c6755a90b4158ba44ceb6fdfb03f84df6a42c
Anh Vuong <vuonga1103@yahoo.com>
2020-06-08 20:02:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,8 @@ def key_for_min_value(name_hash)
   smallest_value = nil
 
   name_hash.each do |key, value|
+    # For the first run, sincce smallest_value is nil, smallest_value is then set to the first value, and the smallest_key is set to the first key
+    # The subsequent values then can be compared to an actual number instead of to nil
     if smallest_value == nil || value < smallest_value
       smallest_key = key
       smallest_value = value
